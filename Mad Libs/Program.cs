a4d4d3a68ba33699ef03d968f74c6b11c152c77f
Tibo Vanslembrouck <vanslembroucktibo1@leerling.sint-rembert.be>
2025-01-29 09:14:14 +0100

    Mad Libs opgelost

--- Mad Libs/Program.cs
+++ Mad Libs/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Mad_Libs
+﻿using System.Threading.Channels;
+
+namespace Mad_Libs
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!!!!!");
+            Console.WriteLine("Wat is je naam?");
+            string? naam;
+                naam = Console.ReadLine();
+            Console.WriteLine("Geef een zelfstandig naamwoord?");
+            string? naamwoord;
+            naamwoord = Console.ReadLine();
+            Console.WriteLine("geef een adjectief?");
+            string? adjectief;
+            adjectief = Console.ReadLine();
+            Console.WriteLine("Geef een werkwoord?");
+            string? werkwoord;
+            werkwoord = Console.ReadLine();
+            Console.WriteLine($"Op een dag ging {naam} naar de AP Hogeschool. Hij zag daar een {adjectief} {naamwoord} en vond dat zo grappig dat hij begon te {werkwoord}.");
         }
     }
 }
