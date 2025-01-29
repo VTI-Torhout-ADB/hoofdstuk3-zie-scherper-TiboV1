1cc3c40a85202875b5a274bed3f8a350548cefce
Tibo Vanslembrouck <vanslembroucktibo1@leerling.sint-rembert.be>
2025-01-29 09:59:48 +0100

    oplossing Systeem informatie

--- Systeem informatie/Program.cs
+++ Systeem informatie/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int? ProcessorCount;
+                ProcessorCount = Environment.ProcessorCount;
+            long? WorkingSet;
+            WorkingSet = Environment.WorkingSet;
+            string? MachineName;
+            MachineName = Environment.MachineName;
+            string? UserName;
+            UserName = Environment.UserName;
+            bool? is64OperatingSystem;
+            is64OperatingSystem = Environment.Is64BitOperatingSystem;
+
+            long? WorkingSetMB;
+            WorkingSetMB = WorkingSet / 1000000;
+            Console.WriteLine($"Systeeminformatie voor admin op damsPowahPC:\r\n---------------------------------------------------\r\n    Aantal processors: {ProcessorCount}\r\n    64-bit besturingssysteem: {is64OperatingSystem}\r\n    Huidige geheugengebruik: {WorkingSetMB}\r\n    MachineName: {MachineName}r\n    UserName {UserName}r\n---------------------------------------------------");
         }
     }
 }
