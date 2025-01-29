f58b22c34b5e88f24fa0727ac78a1e90a7fd823c
Tibo Vanslembrouck <vanslembroucktibo1@leerling.sint-rembert.be>
2025-01-29 09:34:20 +0100

    oplossing Escape conversatie

--- Escape conversatie/Program.cs
+++ Escape conversatie/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string personage1 = "Alice";
+            string personage2 = "Bob";
+
+            string dialoog = $"{personage1}: \"Hoe gaat het met je?\"\r\n    {personage2}: \"Goed, dank je! Hoe gaat het met jou?\"\r\n{personage1}: \"Ook goed, bedankt dat je het vraagt.\"";
+
+            Console.WriteLine(dialoog);
         }
     }
 }
